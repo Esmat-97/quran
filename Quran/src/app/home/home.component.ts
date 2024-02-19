595536ffe9f09ab0c134d31574aaf637852cc51a
mohamedesmat1997 <158211063+Esmat-97@users.noreply.github.com>
2024-02-19 13:14:11 +0200

    add filter

--- Quran/src/app/home/home.component.ts
+++ Quran/src/app/home/home.component.ts
@@ -1,18 +1,21 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { ChildComponent } from '../child/child.component';
+import { headerComponent } from '../header component ';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule,ChildComponent],
+  imports: [CommonModule,ChildComponent,headerComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 
 export class HomeComponent {
   data:any=[];
+  filter:any=[];
   
   constructor(private httpCliet:HttpClient){}
 
@@ -24,7 +27,24 @@ export class HomeComponent {
     .subscribe((data:any)=>{
       console.log(data[0])
       this.data=data;
+      this.filter=this.data
     })
   }
+
+
   
+  handleInput(value:any):void{
+
+    console.log(value);
+      if(value ==" "){
+      this.filter == this.data                           
+                  }
+     else{
+                                           
+        this.filter = this.data.filter((item:any)=>item.sura_name_ar.includes(value) )
+   }
+ 
+ 
+   }    
+
 }
