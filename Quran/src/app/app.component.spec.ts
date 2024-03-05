e1bf445e9fc490c3fa1e234ac6e6b5aa9d5d8fc7
mohamedesmat1997 <158211063+Esmat-97@users.noreply.github.com>
2024-03-05 19:26:20 +0200

    test1

--- Quran/src/app/app.component.spec.ts
+++ Quran/src/app/app.component.spec.ts
@@ -1,29 +1,65 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { Component } from '@angular/core';
+import { Addition } from './Calculator';
+
 
 describe('AppComponent', () => {
+
+  let component =new AppComponent();
+
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
     }).compileComponents();
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
+  
 
-  it(`should have the 'Quran' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('Quran');
-  });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Quran');
-  });
+  it('My testcase',()=>{
+    expect(true).toBe(true);
+      });
+
+
+
+
+
+
+          it('Show  equality result',()=>{
+            var a="one";
+            var b="one";
+            expect(a).toEqual(b);
+              }); 
+
+    
+
+              it('Show  true result',()=>{
+                var a=true;
+                expect(a).toBeTrue();
+              
+                  }); 
+
+
+                  it('Show  false result',()=>{
+                    var a=false;
+                    expect(a).toBeFalse();
+                      }); 
+
+
+
+              it('Show Aler message',()=>{
+                expect(component.ShowMessage("HELLO")).toBe("HELLO");
+                  }); 
+        
+        
+        
+                  it('Show  addition result',()=>{
+                    expect(Addition(10,20)).toBe(30);
+                      }); 
+
+
+                    
+
 });
--- Quran/src/app/app.component.ts
+++ Quran/src/app/app.component.ts
@@ -10,4 +10,8 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'Quran';
+  
+  ShowMessage(msg:string):string{
+return msg;
+  }
 }
